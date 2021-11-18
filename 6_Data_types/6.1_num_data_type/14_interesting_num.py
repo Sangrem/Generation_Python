9497689a22551a06a8f33002751ad3d0779c6373
Alexey Kiselev <89671336668f@gmail.com>
2021-11-18 09:52:30 +0300

    Update 14_interesting_num.py

--- 6_Data_types/6.1_num_data_type/14_interesting_num.py
+++ 6_Data_types/6.1_num_data_type/14_interesting_num.py
@@ -1,4 +1,5 @@
 n = int(input())
+
 n1 = n // 100
 n2 = n // 10 % 10
 n3 = n % 10
@@ -6,6 +7,7 @@ nmax = max(n1, n2, n3)
 nmin = min(n1, n2, n3)
 nmid = (n1 + n2 + n3) - nmax - nmin
 res = nmax - nmin
+
 if res == nmid:
     print("Число интересное")
 else:
