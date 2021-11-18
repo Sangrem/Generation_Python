fc0175e282a39e7dbf7e03cd3641dccde7b1910a
Alexey Kiselev <89671336668f@gmail.com>
2021-11-18 09:51:36 +0300

    Update 12_max_min.py

--- 6_Data_types/6.1_num_data_type/12_max_min.py
+++ 6_Data_types/6.1_num_data_type/12_max_min.py
@@ -1,5 +1,7 @@
 n1, n2, n3, n4, n5 = int(input()), int(input()), int(input()), int(input()), int(input())
+
 nmax = max(n1, n2, n3, n4, n5)
 nmin = min(n1, n2, n3, n4, n5)
+
 print("Наименьшее число =", nmin)
 print("Наибольшее число =", nmax)
