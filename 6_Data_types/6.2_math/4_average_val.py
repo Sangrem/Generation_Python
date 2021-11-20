6bb5bb81b8f1411adb2d3b99d5f8f89c375c986f
Alexey Kiselev <89671336668f@gmail.com>
2021-11-20 15:16:08 +0300

    Update 4_average_val.py

--- 6_Data_types/6.2_math/4_average_val.py
+++ 6_Data_types/6.2_math/4_average_val.py
@@ -1,10 +1,12 @@
 from math import sqrt
 
 a, b = float(input()), float(input())
+
 r1 = (a + b) / 2
 r2 = sqrt(a * b)
 r3 = (2 * a * b) / (a + b)
 r4 = sqrt((a ** 2 + b ** 2) / 2)
+
 print(r1)
 print(r2)
 print(r3)
