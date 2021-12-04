013558c5fae7052cd59b6242b413ec92d8374c53
Alexey Kiselev <89671336668f@gmail.com>
2021-12-04 21:57:52 +0300

    Update 12_how_many_times.py

--- 9_String_data_type/9.1_index/12_how_many_times.py
+++ 9_String_data_type/9.1_index/12_how_many_times.py
@@ -1,10 +1,12 @@
 a = input()
 star = 0
 plus = 0
+
 for i in a:
     if i == '*':
         star += 1
     elif i == '+':
         plus += 1
+
 print('Символ + встречается', plus, 'раз')
 print('Символ * встречается', star, 'раз')
